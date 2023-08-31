6bddf8f3ed7d1c2be70f9acc582164e75420ed10
Simon <floogles71@gmail.com>
2023-08-31 18:21:32 +0200

    tilt hover effect

--- app/components/card/card.tsx
+++ app/components/card/card.tsx
@@ -1,9 +1,11 @@
+"use client"
 import cardStyles from './card.module.css'
+import Tilt from 'react-parallax-tilt';
 
 export default function Card({children, className}: {children: React.ReactNode, className?: string}) {
     return (
-        <div className={'flex flex-col p-5 rounded-md w-1/3 ' + className + ' ' + cardStyles.card}>
+        <Tilt perspective={2000} className={'flex flex-col p-5 rounded-md w-1/3 ' + className + ' ' + cardStyles.card}>
             {children}
-        </div>
+        </Tilt>
     )
 }
